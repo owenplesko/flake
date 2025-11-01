16ed7ab18fadabc3086da9043ff1ea69727ba66e
owen <owenplesko@gmail.com>
2025-11-01 18:31:17 -0400

    firefox2

--- home-manager/firefox.nix
+++ home-manager/firefox.nix
@@ -4,22 +4,39 @@
   config,
   pkgs,
   ...
-}: 
-let
-  profile = "owen";
-in
-{
+} : {
   programs.firefox = {
     enable = true;
     
     profiles = {
-      owen = {
-	name = "${profile}";
+      default = {
+	id = 0;
+	name = "default";
 	isDefault = true;	
-        extensions = [ ]; 
+        settings = {
+	  "browser.startup.homepage" = "https://google.com";
+	};
+	extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+          ublock-origin
+	];
+	bookarks = [
+          {
+	    name = "Nix Resources";
+	    bookmarks = [
+	      {
+	        name = "Home Manager";
+		url = "https://nix-community.github.io/home-manager/options.xhtml";
+	      }
+	      {
+	        name = "Stylix";
+		url = "https://nix-community.github.io/stylix/";
+	      }
+	    ];
+	  }
+	];
       };
     };
   };
 
-  stylix.targets.firefox.profileNames = [ "${profile}" ];
+  stylix.targets.firefox.profileNames = [ "default" ];
 }
