bc2d826e3cb068bd095968db4c5a539e0a2b10d1
Owen Plesko <owenplesko@gmail.com>
2026-05-31 22:15:53 -0400

    2026-05-31 22:15:53

--- hosts/personal-laptop/home.nix
+++ hosts/personal-laptop/home.nix
@@ -1,11 +1,6 @@
-{
-  inputs,
-  config,
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   imports = [
-    ../../modules/home-manager/programs/nvim
+    ../../modules/home-manager/nvim
   ];
 
   programs.home-manager.enable = true;
@@ -17,6 +12,11 @@
     # Scripts
     (writeShellScriptBin "rebuild" ''
       #!${bash}/bin/bash
+      timestamp=$(date "+%Y-%m-%d %H:%M:%S")
+      cd ~/nixos
+      git add .
+      git commit -m "$timestamp $1"
+      git push
       sudo darwin-rebuild switch --flake .#Owens-MacBook
     '')
   ];
--- hosts/server/configuration.nix
+++ hosts/server/configuration.nix
@@ -1,6 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
 {
   inputs,
   pkgs,
@@ -24,11 +21,6 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -53,7 +45,6 @@
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
 
   # Enable the Xfce Desktop Environment.
   services.xserver.desktopManager.xfce.enable = true;
@@ -65,27 +56,13 @@
     variant = "";
   };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
+  # Enable network storage
+  services.nfs.server = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    exports = ''
+      /mnt/media 192.168.1.0/24(rw,sync,no_subtree_check)
+    '';
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users."owen" = {
@@ -100,15 +77,11 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # Allow nix-command and flakes !!
+  # Allow nix-command and flakes
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim
     git
-    #  wget
   ];
 
   # enable remote desktop server
@@ -117,30 +90,5 @@
     openFirewall = true;
   };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "26.05"; # Did you read the comment?
+  system.stateVersion = "26.05";
 }
