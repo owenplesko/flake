7e6870eb1c78f899f18d34d1cbc5c33633df339f
owen <owenplesko@gmail.com>
2026-06-01 12:58:34 -0400

    2026-06-01 12:58:34

--- modules/home-manager/git/default.nix
+++ modules/home-manager/git/default.nix
@@ -5,7 +5,7 @@
     settings = {
       user.name = "owen";
       user.email = "owenplesko@gmail.com";
-      credential.helper = "!f() { echo \"password=$(cat /run/secrets/github/pat)\"; }; f";
+      credential.helper = "!f() { echo \"username=YOUR_GITHUB_USERNAME\"; echo \"password=$(cat /run/secrets/github/pat)\"; }; f";
     };
   };
 }
