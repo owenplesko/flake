32c399dc523aace07641d18a5db355a492fbad5e
owen <owenplesko@gmail.com>
2026-06-01 10:17:06 -0400

    2026-06-01 10:17:06

--- hosts/server/configuration.nix
+++ hosts/server/configuration.nix
@@ -1,11 +1,11 @@
 {
   inputs,
   pkgs,
-  sops,
   ...
 }: {
   imports = [
     inputs.home-manager.nixosModules.home-manager
+    inputs.sops-nix.nixosModules.sops
     ./hardware-configuration.nix
   ];
 
