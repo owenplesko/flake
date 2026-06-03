4e61e02a95e027bd4b2b727ce99a041c47fb80f2
owen <owenplesko@gmail.com>
2026-06-03 14:21:44 -0400

    2026-06-03 14:21:44

--- hosts/personal/configuration.nix
+++ hosts/personal/configuration.nix
@@ -89,13 +89,6 @@
 
   virtualisation.docker.enable = true;
 
-  services.sunshine = {
-    enable = true;
-    autoStart = true; # optional: starts Sunshine automatically on login
-    capSysAdmin = true;
-    openFirewall = true;
-  };
-
   programs.steam = {
     enable = true;
     gamescopeSession.enable = true;
